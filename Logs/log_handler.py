031885c8c9923433974a33b5776f46f370d3e262
Samanth <bloop1508@gmail.com>
2025-08-09 02:01:26 +0530

    Improve code readability

--- Logs/log_handler.py
+++ Logs/log_handler.py
@@ -2,8 +2,12 @@ import csv
 import os
 from datetime import datetime
 
+BASE_DIR= os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+LOGS_DIR= os.path.join(BASE_DIR, "Logs")
+os.makedirs(LOGS_DIR, exist_ok= True)
+
 def log_intrusion(image_filename, alert_status):
-    log_file= os.path.join("Logs", "intruder_log.csv")
+    log_file= os.path.join(LOGS_DIR, "intruder_log.csv")
     log_exists= os.path.exists(log_file)
 
     with open(log_file, mode= 'a', newline= '') as file:
